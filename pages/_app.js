dc197ce42a8afcd708aa6691575838934cb905e3
trentlarkins <trent@theaestheticclient.com>
2025-04-25 12:14:48 -0400

    Add _app.js with Bootstrap import

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
